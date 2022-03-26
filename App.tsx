d077a7628ba4463fbae838ae16dcfe5278d8ae23
Dercio Bucuane <dercio.bucuane@fgh.org.mz>
2022-03-26 13:34:03 +0200

    integrated native base

--- App.tsx
+++ App.tsx
@@ -14,10 +14,11 @@ import {
   ScrollView,
   StatusBar,
   StyleSheet,
-  Text,
   useColorScheme,
   View,
 } from 'react-native';
+import { NativeBaseProvider,Text,Button } from 'native-base';
+
 
 import {
   Colors,
@@ -63,33 +64,10 @@ const App = () => {
   };
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    
+    <NativeBaseProvider>
+       <Button>Testing Text</Button>
+    </NativeBaseProvider>
   );
 };
 
