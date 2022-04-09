66c42d0bf28342f947481da3b480a36f24b5f89c
Dercio Bucuane <dercio.bucuane@fgh.org.mz>
2022-04-09 14:44:00 +0200

    refactored sync object

--- src/database/sync.ts
+++ src/database/sync.ts
@@ -3,28 +3,31 @@ import { database } from './index';
 
 const SYNC_API_URL = 'http://192.168.43.244:8083/sync'; 
 
-export async function sync() {
+export async function sync({ username }) {
     await synchronize({
         database,
         pullChanges: async ({lastPulledAt}) => {
             const response = await fetch(
-              `${SYNC_API_URL}?lastPulledAt=${lastPulledAt}`,
+              `${SYNC_API_URL}?lastPulledAt=${lastPulledAt}&username=${username}`,
             );
+
             if (!response.ok) {
-                console.log(response);
+                
               throw new Error(await response.text());
             }   
       
-            const {changes, timestamp} = await response.json();   
+            const {changes, timestamp} = await response.json();
+       
             return {changes, timestamp};
         },
         pushChanges: async ({changes, lastPulledAt}) => {
-            const response = await fetch(SYNC_API_URL, {
+            const response = await fetch(`${SYNC_API_URL}?username=${username}`, {
               method: 'POST',
               headers: {'Content-Type': 'application/json'},
               body: JSON.stringify({changes, lastPulledAt}),
             });
             if (!response.ok) {
+      
               throw new Error(await response.text());
             }
         },
