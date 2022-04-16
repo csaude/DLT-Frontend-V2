0f6e30a68df5338761bc758ca8a66652cfc8bb08
Gerzélio Saide <gerzeliodomingos@gmail.com>
2022-04-16 11:19:50 +0200

    Remove items in Header (DLT-WEB)

--- web/src/modules/main/header/Header.tsx
+++ web/src/modules/main/header/Header.tsx
@@ -4,9 +4,6 @@ import {useTranslation} from 'react-i18next';
 import {useDispatch, useSelector} from 'react-redux';
 import {toggleControlSidebar, toggleSidebarMenu} from '@app/store/reducers/ui';
 import {Button} from '@app/components';
-import MessagesDropdown from '@app/modules/main/header/messages-dropdown/MessagesDropdown';
-import NotificationsDropdown from '@app/modules/main/header/notifications-dropdown/NotificationsDropdown';
-import LanguagesDropdown from '@app/modules/main/header/languages-dropdown/LanguagesDropdown';
 import UserDropdown from '@app/modules/main/header/user-dropdown/UserDropdown';
 
 const Header = () => {
@@ -43,27 +40,9 @@ const Header = () => {
             <i className="fas fa-bars" />
           </button>
         </li>
-        <li className="nav-item d-none d-sm-inline-block">
-          <Link to="/" className="nav-link">
-            {t('header.label.home')}
-          </Link>
-        </li>
-        <li className="nav-item d-none d-sm-inline-block">
-          <Link to="/" className="nav-link">
-            {t('header.label.contact')}
-          </Link>
-        </li>
       </ul>
       <ul className="navbar-nav ml-auto">
-        <MessagesDropdown />
-        <NotificationsDropdown />
-        <LanguagesDropdown />
         <UserDropdown />
-        <li className="nav-item">
-          <Button className="nav-link" onClick={handleToggleControlSidebar}>
-            <i className="fas fa-th-large" />
-          </Button>
-        </li>
       </ul>
     </nav>
   );
