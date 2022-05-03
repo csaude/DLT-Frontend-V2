d37b062c081be1f45b16633daa4bffb4a69ba612
Dercio Bucuane <dercio.bucuane@fgh.org.mz>
2022-05-03 21:55:00 +0200

    added interv form panel

--- web/src/pages/beneficiaries/components/InterventionForm.tsx
+++ web/src/pages/beneficiaries/components/InterventionForm.tsx
@@ -1,13 +1,32 @@
 import React, { Fragment, useEffect, useState } from 'react'
-import { Drawer, Form, Button, Col, Row, Input, Select, DatePicker, Space } from 'antd';
+import { Drawer, Form, Button, Col, Row, Input, Select, DatePicker, Space, Radio } from 'antd';
 
 const { Option } = Select;
+const { TextArea } = Input;
 
 const areaServicos = [{"id":1,"name": "Serviços Clinicos"},{"id":2,"name": "Serviços Comunitarios"}];
+const options = [
+  { label: 'US', value: 'US' },
+  { label: 'CM', value: 'CM' },
+  { label: 'ES', value: 'ES' },
+];
 
 const InterventionForm = () => {
     const [services, setServices] = React.useState();
     const [interventions, setInterventions] = React.useState();
+
+/*
+    useEffect(() => {
+
+      const fetchData = async () => {
+        const data = await query();
+        setBeneficiaries(data);
+      } 
+  
+      fetchData().catch(error => console.log(error));
+  
+    }, []);
+*/
 
     const onChangeAreaServiço = (value:any) => {
         console.log(value);
@@ -31,79 +50,89 @@ const InterventionForm = () => {
               </Col>
               <Col span={8}>
                 <Form.Item
-                  name="url"
-                  label="Url"
+                  name="service"
+                  label="Serviço *"
                   rules={[{ required: true, message: 'Please enter url' }]}
                 >
-                  
+                  <Select placeholder="Select Serviço" onChange={onChangeAreaServiço}>
+                        {areaServicos.map(item => (
+                            <Option key={item.id}>{item.name}</Option>
+                        ))}
+                  </Select>
                 </Form.Item>
               </Col>
               <Col span={8}>
                 <Form.Item
-                  name="url"
-                  label="Url"
+                  name="subservice"
+                  label="Sub-Serviço/Intervenção"
                   rules={[{ required: true, message: 'Please enter url' }]}
                 >
-                  
+                  <Select placeholder="Select Sub Serviço" onChange={onChangeAreaServiço}>
+                        {areaServicos.map(item => (
+                            <Option key={item.id}>{item.name}</Option>
+                        ))}
+                  </Select>
                 </Form.Item>
               </Col>
             </Row>
-            <Row gutter={16}>
-              <Col span={12}>
+            <Row gutter={8}>
+              <Col span={8}>
                 <Form.Item
-                  name="owner"
-                  label="Owner"
+                  name="entryPoint"
+                  label="Ponto de Entrada *"
                   rules={[{ required: true, message: 'Please select an owner' }]}
                 >
-                  
+                  <Radio.Group
+                    options={options}
+                    defaultValue='US'
+                    optionType="button"
+                  />
                 </Form.Item>
               </Col>
-              <Col span={12}>
+              <Col span={8}>
                 <Form.Item
-                  name="type"
-                  label="Type"
+                  name="location"
+                  label="Localização *"
                   rules={[{ required: true, message: 'Please choose the type' }]}
                 >
-                  
+                  <Select placeholder="Select Localização" onChange={onChangeAreaServiço}>
+                        {areaServicos.map(item => (
+                            <Option key={item.id}>{item.name}</Option>
+                        ))}
+                  </Select>
+                </Form.Item>
+              </Col>
+              <Col span={8}>
+                <Form.Item
+                  name="dataBeneficio"
+                  label="Data Benefício *"
+                  rules={[{ required: true, message: 'Please select an owner' }]}
+                >
+                  <DatePicker style={{width: '100%'}} />
                 </Form.Item>
               </Col>
             </Row>
-            <Row gutter={16}>
-              <Col span={12}>
+            <Row gutter={8}>
+              <Col span={8}>
                 <Form.Item
-                  name="approver"
-                  label="Approver"
-                  rules={[{ required: true, message: 'Please choose the approver' }]}
+                  name="provider"
+                  label="Provedor do Serviço"
+                  rules={[{ required: true, message: 'Nome do Provedor do Serviço' }]}
                 >
-                  
+                  <Input placeholder="Nome do Provedor do Serviço" />
                 </Form.Item>
               </Col>
-              <Col span={12}>
+              <Col span={8}>
                 <Form.Item
-                  name="dateTime"
-                  label="DateTime"
-                  rules={[{ required: true, message: 'Please choose the dateTime' }]}
+                  name="outros"
+                  label="Outras Observações"
+                  rules={[{ required: true, message: 'Please ' }]}
                 >
-                  
-                </Form.Item>
-              </Col>
-            </Row>
-            <Row gutter={16}>
-              <Col span={24}>
-                <Form.Item
-                  name="description"
-                  label="Description"
-                  rules={[
-                    {
-                      required: true,
-                      message: 'please enter url description',
-                    },
-                  ]}
-                >
-                  
+                  <TextArea rows={2} placeholder="Insira as Observações" maxLength={6} />
                 </Form.Item>
               </Col>
             </Row>
+           
           </Form>
     );
 }
