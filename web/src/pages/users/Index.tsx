4561d94ca39e48b7b4beca6ec90dcecb4c3a6dbf
derciobucuane <80683190+derciobucuane@users.noreply.github.com>
2022-06-27 15:46:39 +0200

    Edit User Impl (#38)

--- web/src/pages/users/Index.tsx
+++ web/src/pages/users/Index.tsx
@@ -1,5 +1,4 @@
 import { Card, Table, message, Button, Space, Badge, Form } from 'antd';
-import { useNavigate } from 'react-router-dom';
 import React, { Fragment, useEffect, useState } from 'react';
 import { query } from '../../utils/users';
 import { UserModel } from '../../models/User';
@@ -9,31 +8,39 @@ import { add, edit } from '@app/utils/users';
 
 
 const UsersList: React.FC = () => {
-    const [ users, setUsers ] = useState<UserModel[]>([]);
-    const [ usersModalVisible, setUsersModalVisible] = useState<boolean>(false);
-    const [ selectedUser, setSelectedUser] = useState(undefined);
+    const [users, setUsers] = useState<UserModel[]>([]);
+    const [usersModalVisible, setUsersModalVisible] = useState<boolean>(false);
+    const [selectedUser, setSelectedUser] = useState<any>(undefined);
     const [form] = Form.useForm();
-    
-    const navigate = useNavigate();
 
     useEffect(() => {
         const fetchData = async () => {
-          const data = await query();
-          setUsers(data);
-        } 
-    
+            const data = await query();
+            setUsers(data);
+        }
+
         fetchData().catch(error => console.log(error));
-    
+
     }, []);
 
     const handleUsersModalVisible = (flag?: boolean) => {
+        form.resetFields();
+        setSelectedUser(undefined);
         setUsersModalVisible(!!flag);
     };
 
+    const onEditUser = (record: any) => {
+        //console.log(record);
+        form.resetFields();
+        setUsersModalVisible(true);
+        setSelectedUser(record);
+    };
+
     const handleAdd = () => {
+
         form.validateFields().then(async (values) => {
-            
-            const user: any = {};
+
+            const user: any = selectedUser ? selectedUser : {};
 
             user.surname = values.surname;
             user.name = values.name;
@@ -56,8 +63,23 @@ const UsersList: React.FC = () => {
                 { "id": item }
             ));
 
-            const account =  await add(user);
-            console.log(account);
+            const { data } = selectedUser ? await edit(user) : await add(user);
+
+            if (selectedUser === undefined) {
+                const { data } = await add(user);
+                setUsers(users => [...users, data]);
+
+            } else {
+                const { data } = await edit(user);
+                setUsers(existingItems => {
+                    return existingItems.map((item, j) => {
+                        return item.id === selectedUser.id ?
+                            data : item
+                    })
+                });
+            }
+
+            handleUsersModalVisible(false);
 
             message.success({
                 content: 'Registado com Sucesso!', className: 'custom-class',
@@ -65,81 +87,81 @@ const UsersList: React.FC = () => {
                     marginTop: '10vh',
                 }
             });
-
         })
-        .catch(error => {
-            message.error({
-                content: 'Não foi possivel Registrar Utilizador!', className: 'custom-class',
-                style: {
-                    marginTop: '10vh',
-                }
+            .catch(error => {
+                message.error({
+                    content: 'Não foi possivel Registrar Utilizador!', className: 'custom-class',
+                    style: {
+                        marginTop: '10vh',
+                    }
+                });
             });
-        });
 
-        handleUsersModalVisible(false);
+
     }
 
     const columns = [
-        { title: 'Tipo de Utilizador', dataIndex: '', key: 'type', 
-            render: (text, record)  => record.profiles.description,
+        {
+            title: 'Tipo de Utilizador', dataIndex: '', key: 'type',
+            render: (text, record) => record.profiles.description,
         },
-        { title: 'Estado de Utilizador', dataIndex: '', key: 'status',
+        {
+            title: 'Estado de Utilizador', dataIndex: '', key: 'status',
             render: (text, record) => (
-            
+
                 <Badge
                     className="site-badge-count-109"
                     count={record.status == 1 ? 'activo' : 'Inactivo'}
-                    style={ record.status == 1 ? {backgroundColor: '#52c41a'} :
-                                                {backgroundColor: '#f5222d'}
-                }
-                /> 
+                    style={record.status == 1 ? { backgroundColor: '#52c41a' } :
+                        { backgroundColor: '#f5222d' }
+                    }
+                />
             ),
         },
-        { title: 'Username', dataIndex: 'username', key: 'username'},
-        { title: 'Nome de Utilizador', dataIndex: 'name', key: 'name'},
-        { title: 'Ponto de Entrada', dataIndex: '', key: 'type', 
-            render: (text, record)  => 
-                (record.entryPoint==="1") ?
+        { title: 'Username', dataIndex: 'username', key: 'username' },
+        { title: 'Nome de Utilizador', dataIndex: 'name', key: 'name' },
+        {
+            title: 'Ponto de Entrada', dataIndex: '', key: 'type',
+            render: (text, record) =>
+                (record.entryPoint === "1") ?
                     "Unidade Sanitaria"
-                : 
-                (record.entryPoint==="2") ? 
-                    "Escola"
-                : 
-                    "Comunidade"                                            
-            
+                    :
+                    (record.entryPoint === "2") ?
+                        "Escola"
+                        :
+                        "Comunidade"
+
         },
-        { title: 'Parceiro', dataIndex: '', key: 'type', 
-            render: (text, record)  => record.partners?.name,
+        {
+            title: 'Parceiro', dataIndex: '', key: 'type',
+            render: (text, record) => record.partners?.name,
         },
-        { title: 'Email', dataIndex: 'email', key: 'email'},
-        { title: 'Telefone', dataIndex: 'phoneNumber', key: 'phoneNumber'},
+        { title: 'Email', dataIndex: 'email', key: 'email' },
+        { title: 'Telefone', dataIndex: 'phoneNumber', key: 'phoneNumber' },
         {
             title: 'Action',
             dataIndex: '',
             key: 'x',
             render: (text, record) => (
-              <Space>
-                <Button type="primary" icon={<EyeOutlined />} onClick={() => navigate("/usersView", { state: { user: record } } )} >
-                </Button>
-                <Button type="primary" icon={<EditOutlined />} onClick={() => navigate("/usersForm", { state: { user: record } } )} >
-                </Button>
-              </Space>
+                <Space>
+                    <Button type="primary" icon={<EditOutlined />} onClick={() => onEditUser(record)} >
+                    </Button>
+                </Space>
             ),
         },
     ];
 
-    
     return (
         <>
-            <Card  bordered={false} style={{marginBottom:'10px', textAlign:"center", fontWeight:"bold", color:"#17a2b8"}} >
+            <Card bordered={false} style={{ marginBottom: '10px', textAlign: "center", fontWeight: "bold", color: "#17a2b8" }} >
                 SISTEMA INTEGRADO DE CADASTRO DE ADOLESCENTES E JOVENS
             </Card>
-            <Card title="Lista de Utilizadores" bordered={false} headStyle={{color:"#17a2b8"}}
+            <Card title="Lista de Utilizadores" bordered={false} headStyle={{ color: "#17a2b8" }}
                 extra={
                     <Space>
-                      <Button type="primary" icon={<PlusOutlined />} onClick={()=>handleUsersModalVisible(true)}>
-                        Adicionar Utilizador
-                      </Button>
+                        <Button type="primary" icon={<PlusOutlined />} onClick={() => handleUsersModalVisible(true)}>
+                            Adicionar Utilizador
+                        </Button>
                     </Space>
                 }
             >
@@ -150,7 +172,7 @@ const UsersList: React.FC = () => {
                     bordered
                 />
             </Card>
-            <UsersForm form={form} user={selectedUser} modalVisible={usersModalVisible} handleModalVisible={handleUsersModalVisible} handleAdd={handleAdd}/>
+            <UsersForm form={form} user={selectedUser} modalVisible={usersModalVisible} handleModalVisible={handleUsersModalVisible} handleAdd={handleAdd} />
         </>
     );
 }
