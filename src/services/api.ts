029ddf84b554253da3a73158b045bf144efb62b1
Gerzelio Saide <gerzeliodomingos@gmail.com>
2022-07-06 11:48:40 +0200

    Add endpoint change password in api

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,6 @@
-const URL = "http://10.10.2.119:8083";
+const URL = "http://10.10.2.194:8083";
 
 export const LOGIN_API_URL = `${URL}/api/login`;
 export const SYNC_API_URL = `${URL}/sync`; 
+export const CHANGE_PASSWORD_URL = `${URL}/api/users/change-password`; 
 export const UPDATE_PASSWORD_URL = `${URL}/users/update-password`; 
