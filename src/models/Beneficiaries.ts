89a113d2c549cd7f95c89a07b9e8094e827be9d4
Dercio Bucuane <dercio.bucuane@fgh.org.mz>
2022-07-21 15:29:47 +0200

    refactored Beneficiary model to add missing fields

--- src/models/Beneficiaries.ts
+++ src/models/Beneficiaries.ts
@@ -25,6 +25,7 @@ export interface BeneficiariesModel {
     neighbourhood_id?: any,
     us_id?: any,
     status?: any
+    references?: any
 }
 
 export default class Beneficiarie extends Model {
@@ -34,24 +35,45 @@ export default class Beneficiarie extends Model {
     @text("surname") surname;
     @text("name") name;
     @text("nick_name") nick_name;
-    @text("date_of_birth") date_of_birth;
-    @field("gender") gender;
+    @text("organization_id") organization_id;
+    @field("date_of_birth") date_of_birth;
+    @text("gender") gender;
     @text("address") address;
     @text("phone_number") phone_number;
     @text("e_mail") e_mail;
-    @field("lives_with") lives_with;
-    @field("is_orphan") is_orphan;
     @text("via") via;
     @field("partner_id") partner_id;
-    @field("is_student") is_student;
-    @text("grade") grade;
-    @text("school_name") school_name;
-    @field("is_deficient") is_deficient;
-    @field("deficiency_type") deficiency_type;
     @field("entry_point") entry_point;
     @field("neighbourhood_id") neighbourhood_id;
     @field("us_id") us_id;
     @field("status") status;
+    @field("online_id") online_id;
+    @field("vblt_lives_with") vblt_lives_with;
+    @field("vblt_is_orphan") vblt_is_orphan;
+    @field("vblt_is_student") vblt_is_student;
+    @field("vblt_school_grade") vblt_school_grade;
+    @field("vblt_school_name") vblt_school_name;
+    @field("vblt_is_deficient") vblt_is_deficient;
+    @field("vblt_deficiency_type") vblt_deficiency_type;
+    @field("vblt_married_before") vblt_married_before;
+    @field("vblt_pregnant_before") vblt_pregnant_before;
+    @field("vblt_children") vblt_children;
+    @field("vblt_pregnant_or_breastfeeding") vblt_pregnant_or_breastfeeding;
+    @field("vblt_is_employed") vblt_is_employed;
+    @field("vblt_tested_hiv") vblt_tested_hiv;
+    @field("vblt_sexually_active") vblt_sexually_active;
+    @field("vblt_multiple_partners") vblt_multiple_partners;
+    @field("vblt_is_migrant") vblt_is_migrant;
+    @field("vblt_trafficking_victim") vblt_trafficking_victim;
+    @field("vblt_sexual_exploitation") vblt_sexual_exploitation;
+    @field("vblt_sexploitation_time") vblt_sexploitation_time;
+    @field("vblt_vbg_victim") vblt_vbg_victim;
+    @field("vblt_vbg_type") vblt_vbg_type;
+    @field("vblt_vbg_time") vblt_vbg_time;
+    @field("vblt_alcohol_drugs_use") vblt_alcohol_drugs_use;
+    @field("vblt_sti_history") vblt_sti_history;
+    @field("vblt_sex_worker") vblt_sex_worker;
+    @field("vblt_house_sustainer") vblt_house_sustainer;
+    @field("references") references;
     
 }
-
