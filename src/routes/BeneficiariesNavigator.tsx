7f97ac6c543eda3304f07fcde1bf6732e1516902
Dercio Bucuane <dercio.bucuane@fgh.org.mz>
2022-07-21 09:18:00 +0200

    added refactoring on Navigator

--- src/routes/BeneficiariesNavigator.tsx
+++ src/routes/BeneficiariesNavigator.tsx
@@ -2,7 +2,8 @@ import React from 'react';
 import {Text} from 'react-native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import BeneficiariesListScreen from '../screens/Beneficiarias';
-import BeneficiariesViewScreen from '../screens/Beneficiarias/components/view';
+//import BeneficiariesViewScreen from '../screens/Beneficiarias/components/view';
+import BeneficiariesViewStack from '../screens/Beneficiarias/components/BeneficiariesViewStack';
 import BeneficiarieServiceForm from '../screens/Beneficiarias/components/intervention';
 
 const BeneficiaryStack = createNativeStackNavigator();
@@ -12,7 +13,7 @@ const BeneficiariesNavigator: React.FC = () => {
   return (
       <BeneficiaryStack.Navigator initialRouteName="BeneficiariesList"  screenOptions={{headerShown:false}}>
         <BeneficiaryStack.Screen name="BeneficiariesList" component={BeneficiariesListScreen} />
-        <BeneficiaryStack.Screen name="BeneficiariesView" component={BeneficiariesViewScreen} options={{
+        <BeneficiaryStack.Screen name="BeneficiariesView" component={BeneficiariesViewStack} options={{
                     headerTitle: (props) => (
                       <Text {...props} style={{ color: 'black', fontWeight: 'bold' }}>
                         back
