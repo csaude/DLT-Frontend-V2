e88c43249ddbd1a7a935dc5fffa54e997ffb4d06
Dercio Bucuane <dercio.bucuane@fgh.org.mz>
2022-07-27 14:26:07 +0200

    added References and ReferencesServices to the sync mobile

--- src/models/index.ts
+++ src/models/index.ts
@@ -9,9 +9,10 @@ import Sub_services from './Sub_services'
 import Neighborhoods from './Neighborhoods'
 import Beneficiaries_interventions from './Beneficiaries_interventions'
 import References from './References'
+import References_Services from './References_Services'
 import Sequences from './Sequences'
 
 export const dbModels = [Locality, Partner, Profile, Us, User, 
                         Beneficiaries, Services, Sub_services,
                         Neighborhoods, 
-                        Beneficiaries_interventions, References, Sequences];
+                        Beneficiaries_interventions, References, References_Services, Sequences];
