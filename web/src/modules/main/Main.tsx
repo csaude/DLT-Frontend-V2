da10bd81f4a48156d892c5115e60f2a28bf62081
derciobucuane <80683190+derciobucuane@users.noreply.github.com>
2022-08-17 11:53:47 +0200

    fix img url for deploy (#96)

--- web/src/modules/main/Main.tsx
+++ web/src/modules/main/Main.tsx
@@ -91,7 +91,7 @@ const Main = () => {
         <div className="preloader flex-column justify-content-center align-items-center">
           <img
             className="animation__shake"
-            src="/img/logo.png"
+            src="img/logo.png"
             alt="AdminLTELogo"
             height="60"
             width="60"
