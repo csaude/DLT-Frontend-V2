2f47f576947a0a933bd90882343774a75f909b4e
“Francisco <macuacuafrancisco@gmail.com>
2022-11-23 13:03:30 +0200

    New DLT: Fix User com sessao, Mas com user undefined e perfil undefined

--- web/src/modules/main/header/user-dropdown/UserDropdown.tsx
+++ web/src/modules/main/header/user-dropdown/UserDropdown.tsx
@@ -19,8 +19,7 @@ const UserDropdown = () => {
   const navigate = useNavigate();
   const [t] = useTranslation();
   const dispatch = useDispatch();
-  const user = useSelector((state: any) => state.auth.currentUser);
-  const currentUser = useSelector((state: any) => state.auth.user);
+  const currentUser = useSelector((state: any) => state.auth.currentUser);
   const [dropdownOpen, setDropdownOpen] = useState(false);
 
   const logOut = (event: any) => {
@@ -44,7 +43,7 @@ const UserDropdown = () => {
       menuContainerTag="ul"
       buttonTemplate={
         <StyledUserImage
-          src={user.picture || 'img/default-profile.png'}
+          src={currentUser.picture || 'img/default-profile.png'}
           className="user-image img-circle elevation-2"
           alt="User"
         />
@@ -53,7 +52,7 @@ const UserDropdown = () => {
         <>
           <li className="user-header bg-info">
             <img
-              src={user.picture || 'img/default-profile.png'}
+              src={currentUser.picture || 'img/default-profile.png'}
               className="img-circle elevation-2"
               alt="User"
             />
