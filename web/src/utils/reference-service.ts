3d96fadd96596997e51caa693746b88b52c69f51
macuacuafrancisco <macuacuafrancisco@gmail.com>
2023-02-01 22:07:04 +0200

    REFACTORY WITH MIGRATED DATA [List References Fixed] (#213)

--- web/src/utils/reference-service.ts
+++ web/src/utils/reference-service.ts
@@ -4,10 +4,10 @@ import { create, select, update } from './crud';
 export async function query(payload?: any) {
     let url: string;
     if (payload.profile){
-      url = '/api/beneficiary-intervention?'.concat(stringify(payload));
+      url = '/api/reference-service?'.concat(stringify(payload));
     }
     else {
-      url = '/api/beneficiary-intervention/byBeneficiaryId/' + payload;
+      url = '/api/reference-service/byReferenceId/' + payload;
     }
 
     const res = await select(url);
