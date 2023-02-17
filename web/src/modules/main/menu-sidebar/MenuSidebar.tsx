52e42c4d15841871781f9bb7bbbef092cf251746
macuacuafrancisco <macuacuafrancisco@gmail.com>
2023-02-17 08:53:27 +0200

    Optimizing Performance / Change Beneficiaries and references Totals Implementation (#223)

--- web/src/modules/main/menu-sidebar/MenuSidebar.tsx
+++ web/src/modules/main/menu-sidebar/MenuSidebar.tsx
@@ -2,8 +2,8 @@ import React, {useEffect, useState} from 'react';
 import {useDispatch, useSelector} from 'react-redux';
 import {Link} from 'react-router-dom';
 import {MenuItem} from '@components';
-import { query as beneficiaryQuery } from '../../../utils/beneficiary';
-import { query as referenceQuery } from '../../../utils/reference';
+import { queryCount as beneficiaryQueryCount } from '../../../utils/beneficiary';
+import { queryCount as referenceQueryCount } from '../../../utils/reference';
 import { query as queryUser } from '../../../utils/users';
 import { getUserParams } from '@app/models/Utils';
 import { getReferencesTotal } from '../../../store/actions/reference';
@@ -105,10 +105,10 @@ const MenuSidebar = () => {
 
   const getTotals = async () =>{
       const user = await queryUser(localStorage.user);
-      const beneficiaryData = await beneficiaryQuery(getUserParams(user));
-      const referenceData = await referenceQuery();
-      dispatch(getBeneficiaryTotal(beneficiaryData.length))
-      dispatch(getReferencesTotal(referenceData.length))
+      const beneficiaryTotal = await beneficiaryQueryCount(getUserParams(user));
+      const referenceTotal = await referenceQueryCount(user.id);
+      dispatch(getBeneficiaryTotal(beneficiaryTotal))
+      dispatch(getReferencesTotal(referenceTotal))
   }
 
   useEffect(()=>{
