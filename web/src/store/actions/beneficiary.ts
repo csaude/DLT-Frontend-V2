bc6d6bb263bce5cb961743e2728b438e73644787
“Francisco <macuacuafrancisco@gmail.com>
2023-05-16 15:29:50 +0200

    Rename Service

--- web/src/store/actions/beneficiary.ts
+++ web/src/store/actions/beneficiary.ts
@@ -1,6 +1,6 @@
 import { GET_BENEFICIARIES_TOTAL } from "./types";
 
-export const getBeneficiaryTotal = (total) => async (dispatch) => {
+export const getBeneficiariesTotal = (total) => async (dispatch) => {
     dispatch({
       type: GET_BENEFICIARIES_TOTAL,
       payload: total,
