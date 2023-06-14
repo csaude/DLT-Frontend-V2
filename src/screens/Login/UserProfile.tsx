11afe343723006bbae3bf5ef30770195d78af226
Gerzélio Domingos Saide <gerzeliodomingos@gmail.com>
2023-06-14 12:54:30 +0200

    Show locality in profileUser

--- src/screens/Login/UserProfile.tsx
+++ src/screens/Login/UserProfile.tsx
@@ -1,70 +1,31 @@
-import React, { useState, useEffect, useContext } from "react";
+import React, { useContext } from "react";
 import { View, KeyboardAvoidingView, ScrollView,
-    TextInput, TouchableOpacity, Platform,
     Text} 
     from 'react-native';
-import { Box, HStack, AspectRatio, Center, 
-    Image, Stack, Heading, Divider, Avatar, 
-    Icon, Flex, Spacer, VStack, Button} 
+import { Box, Heading, Divider, Avatar, 
+    Icon, Flex } 
     from "native-base";
-import { parse } from 'qs';
 import { Ionicons } from "@native-base/icons";
-import { navigate } from "../../routes/NavigationRef";
 import { database } from '../../database';
-import { Q } from "@nozbe/watermelondb";
 import styles from "./styles";
 import { Context } from "../../routes/DrawerNavigator";
-import { useSelector } from "react-redux";
-import { RootState } from "../../store";
 import withObservables from "@nozbe/with-observables";
 
-const UserProfile: React.FC = ({ route , users}:any) => {
-    // const {user, profile, locality, partner, us} = route.params;
-
+const UserProfile: React.FC = ({ profiles , users, provinces, districts, localities, us }:any) => {
+    
     const loggedUser: any = useContext(Context);
-    // const user = loggedUser;
-    // const userDetails = useSelector((state: RootState) => state.auth.userDetails);
-    // const userDetailsCollection = database.get('users');
-    // const [user, setUser] = useState<any>([]);
-
-    const firstLogin = loggedUser?.entry_point === undefined ? "1" : "2";
+    
     const user = users.filter((e) => {
-        return e?._raw.online_id == (firstLogin === '1'? loggedUser?.id : loggedUser?.online_id)
+        return e?._raw.online_id == (loggedUser?.entry_point === undefined ? loggedUser?.id : loggedUser?.online_id)
     })[0]?._raw;
-    // const userDetail = userDetailsCollection.query(
-    //     Q.where('online_id', parseInt(firstLogin === '1'? userOn?.id : userOn?.online_id))
-    // ).fetch();
-    // const getUser = (user_id: any) => {
-    //     const localUser = users.filter((e) => {
-    //         return e?._raw.online_id == user_id
-    //     })[0]?._raw;
-    //     // setUser(localUser);
-    // }
-     
-    // const getUser = async (currentUserId) =>{
-        
-    //     const userDetailsCollection = database.get('users')
-    //     // const beneficiariesCollection = database.get("beneficiaries")
+    const profile = profiles.filter((e) => {
+        return e?._raw.online_id == user?.profile_id
+    })[0]?._raw;
 
-    //     const userDetailsQ = await userDetailsCollection.query(
-    //             Q.where('online_id', parseInt(currentUserId))
-    //         ).fetch();
-    //     const userDetailsRaw = userDetailsQ[0]?._raw
+    const uss = us.filter((e) => {
+        return e._raw.online_id = user?.us_ids
+    })[0]?._raw;
    
-    //     // const oneUser = await beneficiariesCollection.query(Q.where('bar', 1)).fetch()
-    //             // setUserBeneficiaries(beneficiaries)
-    // }
-
-    // console.log(getUser(firstLogin === '1'? loggedUser?.id : loggedUser?.online_id));
-    // console.log(users.__changes);
-    // setUser(getUser(firstLogin === '1'? user?.id : user?.online_id));
-    console.log("==================================================================");
-    console.log(user);
-    console.log("==================================================================");
-    // console.log(userDetails);
-    // console.log("==================================================================");
-   
-
     return (
         <KeyboardAvoidingView  style={styles.background}>
             <ScrollView>
@@ -77,13 +38,14 @@ const UserProfile: React.FC = ({ route , users}:any) => {
                             <Box style={styles.userText}>     
                                 <Text>{ user?.username }</Text> 
                                 <Heading style={styles.username}>{ user?.name } { user?.surname }</Heading>  
-                                <Text>{ user?.email }</Text>     
-                                <Text>{ firstLogin === '1'? user?.id : user?.online_id }</Text>                                                                                                                                  
+                                <Text>{ user?.email }</Text>                                                                                                                                
                             </Box> 
                         </Box>
                         <Text style={styles.txtLabel}>Detalhes do Utilizador</Text>
                         <Divider />
                         <Flex direction="column" mb="2.5" mt="1.5" _text={{color: "coolGray.800"}}>
+                                
+                            <Text> <Text style={styles.txtLabel}>Perfil: </Text> {profile?.name}</Text>
                                 
                             <Text> <Text style={styles.txtLabel}>Parceiro: </Text> {user?.organization_name} </Text>
 
@@ -100,12 +62,12 @@ const UserProfile: React.FC = ({ route , users}:any) => {
                                     "Escola"                                            
                                 }  
                             </Text>
-                                
-                            {/* <Text> <Text style={styles.txtLabel}>Localidade: </Text> {locality}</Text>
-                                
-                            <Text> <Text style={styles.txtLabel}>US: </Text> {us}</Text>
-                                
-                            <Text> <Text style={styles.txtLabel}>Perfil: </Text> {profile}</Text> */}
+
+                            <Text> <Text style={styles.txtLabel}>Província(s): </Text> {provinces?.map(p => p.name+', ')} </Text>
+                            
+                            <Text> <Text style={styles.txtLabel}>Distrito(s): </Text> {districts?.map(d => d.name+', ')} </Text>
+                            <Text> <Text style={styles.txtLabel}>Posto(s) Administrativo(s): </Text> {localities?.map(d => d.name+', ')} </Text>
+                            <Text> <Text style={styles.txtLabel}>Alocação: </Text> { uss?.name } </Text>
 
                         </Flex>
                         <Divider />
@@ -121,7 +83,22 @@ const UserProfile: React.FC = ({ route , users}:any) => {
 const enhance = withObservables([], () => ({
     users: database.collections
       .get("users")
-      .query().observe(),
+      .query(),
+    profiles: database.collections
+      .get("profiles")
+      .query(),
+    provinces: database.collections
+      .get("provinces")
+      .query(),
+    districts: database.collections
+      .get("districts")
+      .query(),
+    localities: database.collections
+      .get("localities")
+      .query(),
+    us: database.collections
+      .get("us")
+      .query(),
 }));
 
 export default enhance(UserProfile);
