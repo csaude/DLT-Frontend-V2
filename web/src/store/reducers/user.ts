e0d8b6b0218b0ff9c1dafde3d3af7996ab9c4633
macuacuafrancisco <macuacuafrancisco@gmail.com>
2023-08-30 13:27:35 +0200

    DLT 2.0: Beneficiary loading improvement (#360)

--- web/src/store/reducers/user.ts
+++ web/src/store/reducers/user.ts
@@ -2,6 +2,7 @@ import { GET_USERNAMES, LOAD_REFERERS } from "../actions/types";
 
 const initialState = {
   users: [],
+  loadedUsers: [],
   referers: [],
 };
 
@@ -9,11 +10,20 @@ function userReducer(state = initialState, action) {
   const { type, payload } = action;
 
   switch (type) {
-    case GET_USERNAMES:
+    case GET_USERNAMES: {
+      const loadedUsers: any = [];
+      for (const [userId, username] of payload) {
+        loadedUsers.push({
+          id: userId,
+          username: username,
+        });
+      }
       return {
         ...state,
         users: payload,
+        loadedUsers: loadedUsers,
       };
+    }
     case LOAD_REFERERS:
       return {
         ...state,
