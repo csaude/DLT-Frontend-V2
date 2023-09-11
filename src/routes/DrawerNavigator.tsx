d093628f9899e486f684671e123a7f1284460d9f
Francisco Macuacua <macuacuafrancisco@gmail.com>
2023-09-11 12:10:49 +0200

    App Information -  Version Number, Build Date, ChangeLog

--- src/routes/DrawerNavigator.tsx
+++ src/routes/DrawerNavigator.tsx
@@ -182,17 +182,19 @@ const DrawerNavigation: React.FC = ({ route }: any) => {
         <VStack>
           <Text style={{ fontWeight: "bold", color: "#424345" }}>
             {label}
-            <Badge // bg="red.400"
-              colorScheme={total > 0 ? "info" : "danger"}
-              rounded="full"
-              variant="solid"
-              alignSelf="flex-end"
-              _text={{
-                fontSize: 12,
-              }}
-            >
-              {total}
-            </Badge>
+            {total >= 0 && (
+              <Badge // bg="red.400"
+                colorScheme={total > 0 ? "info" : "danger"}
+                rounded="full"
+                variant="solid"
+                alignSelf="flex-end"
+                _text={{
+                  fontSize: 12,
+                }}
+              >
+                {total}
+              </Badge>
+            )}
           </Text>
         </VStack>
       </Box>
@@ -271,16 +273,18 @@ const DrawerNavigation: React.FC = ({ route }: any) => {
           name="Users"
           component={UsersNavigator}
           options={{
-            title: "Perfil",
+            title: "",
             headerTitle: "",
+            drawerIcon: () => <ItemBadge label="Perfil" total={-1} />,
           }}
         />
         <Drawer.Screen
           name="Info"
           component={AppInfoScreen}
           options={{
-            title: "App Information",
+            title: "",
             headerTitle: "",
+            drawerIcon: () => <ItemBadge label="App Information" total={-1} />,
           }}
         />
       </Drawer.Navigator>
