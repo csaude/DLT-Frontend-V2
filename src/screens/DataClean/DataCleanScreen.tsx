633fc511696cc33fd930345668ede0c148cd233c
Gerzelio Saide <gerzeliodomingos@gmail.com>
2024-06-04 00:32:17 +0200

    add toast

--- src/screens/DataClean/DataCleanScreen.tsx
+++ src/screens/DataClean/DataCleanScreen.tsx
@@ -1,6 +1,6 @@
 import React, { useCallback, useEffect, useState } from "react";
 import { View, KeyboardAvoidingView, ScrollView, Text, GestureResponderEvent } from "react-native";
-import { Button, Divider, Flex, FormControl, Radio, Stack } from "native-base";
+import { Alert, Button, Divider, Flex, FormControl, HStack, Radio, Stack, VStack, useToast, Text as Text1 } from "native-base";
 import Spinner from "react-native-loading-spinner-overlay/lib";
 import styles from "./styles";
 import { useFormik } from "formik";
@@ -8,10 +8,38 @@ import { useFormik } from "formik";
 const DatacleanScreen: React.FC = () => {
 
 
+  const toast = useToast();
+  
+  const [errors, setErrors] = useState(false);
   const [loading, setLoading] = useState(false);
   
   const handleSubmit = async () => {
-    console.log("Testando!!!");
+
+    console.log(formik.values.data_clean);
+
+    const errorsList = validate(formik.values);
+    const hasErrors = JSON.stringify(errorsList) !== "{}";
+
+    if (hasErrors) {
+      setErrors(true);
+      formik.setErrors(errorsList);
+
+      toast.show({
+        placement: "top",
+        render: () => {
+          return <ErrorHandler />;
+        },
+      });
+      
+    } else {
+      setErrors(false);
+      toast.show({
+        placement: "top",
+        render: () => {
+          return <InfoHandler />;
+        },
+      });
+    }
   }
 
   const formik = useFormik({
@@ -57,9 +85,9 @@ const DatacleanScreen: React.FC = () => {
             >
               <Text>
                 {" "}
-                <Text style={styles.txtLabel}>
+                {/* <Text style={styles.txtLabel}>
                   Seleccione a opçao
-                </Text>
+                </Text> */}
               </Text>
 
 
@@ -68,7 +96,7 @@ const DatacleanScreen: React.FC = () => {
                   isRequired
                   isInvalid={"data_clean" in formik.errors}
                 >
-                  {/* <FormControl.Label>Seleccione a opçao</FormControl.Label> */}
+                  <FormControl.Label>Seleccione a opçao</FormControl.Label>
                   <Radio.Group
                     value={formik.values.data_clean + ""}
                     onChange={(itemValue) => {
@@ -88,7 +116,7 @@ const DatacleanScreen: React.FC = () => {
                     >
                       <Radio
                         key="defi1"
-                        value="1"
+                        value="0"
                         colorScheme="green"
                         size="md"
                         my={1}
@@ -97,7 +125,7 @@ const DatacleanScreen: React.FC = () => {
                       </Radio>                      
                       <Radio
                         key="defi2"
-                        value="0"
+                        value="1"
                         colorScheme="green"
                         size="md"
                         my={1}
@@ -211,6 +239,52 @@ const DatacleanScreen: React.FC = () => {
         </View>
       </ScrollView>
     </KeyboardAvoidingView>
+  );
+};
+
+
+const InfoHandler: React.FC = () => {
+  return (
+    <>
+      <Alert w="100%" variant="left-accent" colorScheme="success" status="success">
+        <VStack space={2} flexShrink={1} w="100%">
+          <HStack
+            flexShrink={1}
+            space={2}
+            alignItems="center"
+            justifyContent="space-between"
+          >
+            <HStack space={2} flexShrink={1} alignItems="center">
+              <Alert.Icon />
+              <Text1 color="coolGray.800">Limpeza de dados terminada com sucesso!</Text1>
+            </HStack>
+          </HStack>
+        </VStack>
+      </Alert>
+    </>
+  );
+};
+
+
+const ErrorHandler: React.FC = () => {
+  return (
+    <>
+      <Alert w="100%" variant="left-accent" colorScheme="success" status="error">
+        <VStack space={2} flexShrink={1} w="100%">
+          <HStack
+            flexShrink={1}
+            space={2}
+            alignItems="center"
+            justifyContent="space-between"
+          >
+            <HStack space={2} flexShrink={1} alignItems="center">
+              <Alert.Icon />
+              <Text1 color="coolGray.800">Nenhuma opçao Solicitado!</Text1>
+            </HStack>
+          </HStack>
+        </VStack>
+      </Alert>
+    </>
   );
 };
 
