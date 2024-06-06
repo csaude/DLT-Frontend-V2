6fe09e4b1a2741d4b679f9f9ca2bbd30323b5404
Hamilton Mutaquiha <hamilton.mutaquiha@gmail.com>
2024-06-06 09:34:52 +0200

    keep user's last ligin date

--- src/models/UserDetails.ts
+++ src/models/UserDetails.ts
@@ -9,6 +9,7 @@ export default class UserDetails extends Model {
   @text("localities") localities;
   @text("uss") uss;
   @field("user_id") user_id;
+  @field("last_login_date") last_login_date;
   @field("password_last_change_date") password_last_change_date;
   @field("profile_id") profile_id;
   @field("entry_point") entry_point;
