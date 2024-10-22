54644adedb1d380b91a4ee99de31971d97be7c84
Francisco Macuacua <macuacuafrancisco@gmail.com>
2024-10-22 08:51:01 +0200

    Fix - remove edit call method when login

--- web/src/modules/login/Login.tsx
+++ web/src/modules/login/Login.tsx
@@ -15,7 +15,6 @@ import * as Yup from "yup";
 import { Form, InputGroup } from "react-bootstrap";
 import * as AuthService from "../../services/auth";
 import { verifyUserByUsername } from "../../utils/login";
-import { edit } from "@app/utils/users";
 
 const Login = () => {
   const [isAuthLoading, setAuthLoading] = useState(false);
@@ -48,7 +47,6 @@ const Login = () => {
       dispatch(loginUser(data));
       localStorage.setItem("dateCreated", user?.dateCreated);
       user.lastLoginDate = new Date();
-      await edit(user);
       navigate("/");
     } catch (error) {
       const errSt = JSON.stringify(error);
