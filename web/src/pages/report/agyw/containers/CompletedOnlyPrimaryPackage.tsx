9bf603722e0d07ab63f7617f957372def1a69f29
Francisco Macuacua <macuacuafrancisco@gmail.com>
2025-02-26 11:22:51 +0200

    [ AGYW_PREV ] : Fix Data Fetch Time + click validation

--- web/src/pages/report/agyw/containers/CompletedOnlyPrimaryPackage.tsx
+++ web/src/pages/report/agyw/containers/CompletedOnlyPrimaryPackage.tsx
@@ -299,17 +299,17 @@ const CompletedOnlyPrimaryPackage = ({ districtId }) => {
 
   const handleOnCLick = (e) => {
     const elements = extractElements(arrBeneficiaries);
-    dispatch(
-      loadBeneficiariesIds({ ids: elements, title: title_pt, total: total })
-    );
 
-    if (e.type === "click") {
+    if (e.button === 0) {
+      dispatch(
+        loadBeneficiariesIds({ ids: elements, title: title_pt, total: total })
+      );
       console.log("Left click");
       navigate("/viewAgyw");
     } else {
       console.log("Right click");
       e.preventDefault();
-      if (e.type !== "contextmenu") handleGenerateXLSXReport();
+      if (e.type !== "contextmenu") handleGenerateXLSXReport(elements);
     }
   };
 
@@ -348,21 +348,21 @@ const CompletedOnlyPrimaryPackage = ({ districtId }) => {
       enrollmentTime,
       ageRange
     );
-    dispatch(
-      loadBeneficiariesIds({
-        ids: elements,
-        title: title_pt,
-        total: total,
-      })
-    );
 
-    if (e.type === "click") {
+    if (e.button === 0) {
+      dispatch(
+        loadBeneficiariesIds({
+          ids: elements,
+          title: title_pt,
+          total: total,
+        })
+      );
       console.log("Left click");
       navigate("/viewAgyw");
     } else {
       console.log("Right click");
       e.preventDefault();
-      if (e.type !== "contextmenu") handleGenerateXLSXReport();
+      if (e.type !== "contextmenu") handleGenerateXLSXReport(elements);
     }
   };
 
@@ -384,27 +384,25 @@ const CompletedOnlyPrimaryPackage = ({ districtId }) => {
 
   const handleOnSubTotalClick = (e, enrollmentTime: string, total: number) => {
     const elements = extractSubTotalElements(arrBeneficiaries, enrollmentTime);
-    dispatch(
-      loadBeneficiariesIds({
-        ids: elements,
-        title: title_pt,
-        total: total,
-      })
-    );
 
-    if (e.type === "click") {
+    if (e.button === 0) {
+      dispatch(
+        loadBeneficiariesIds({
+          ids: elements,
+          title: title_pt,
+          total: total,
+        })
+      );
       console.log("Left click");
       navigate("/viewAgyw");
     } else {
       console.log("Right click");
       e.preventDefault();
-      if (e.type !== "contextmenu") handleGenerateXLSXReport();
+      if (e.type !== "contextmenu") handleGenerateXLSXReport(elements);
     }
   };
 
-  async function handleGenerateXLSXReport() {
-    const beneficiariesIds = beneficiariesIdsSelector.slice(); // Copy the array
-
+  async function handleGenerateXLSXReport(beneficiariesIds) {
     setDataLoading(true);
     try {
       const response = await getAgywPrevBeneficiariesReportGenerated(
